9420f316ddb1985d94afda6853a2672e60826fb0
Dzaky Mohammad <dzdzaakk@gmail.com>
2023-05-06 23:50:20 +0700

    add fields on table class rooms

---
+++ database/migrations/20230505130426_create_class_rooms_table.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE class_roooms(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `product_id` INT NOT NULL,
+    `user_id` INT NOT NULL,
+    `created_by` INT NULL,
+    `updated_by` INT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NULL,
+    `deleted_at` TIMESTAMP NULL,
+    PRIMARY KEY(`id`)
+    CONSTRAINT FK_class_rooms_product_id FOREIGN KEY (`product_id`) REFERENCES products(`id`) ON DELETE SET NULL, 
+    CONSTRAINT FK_class_rooms_user_id FOREIGN KEY (`user_id`) REFERENCES users(`id`) ON DELETE SET NULL 
+    CONSTRAINT FK_class_rooms_created_by FOREIGN KEY (`created_by`) REFERENCES admins(`id`) ON DELETE SET NULL, 
+    CONSTRAINT FK_class_rooms_updated_by FOREIGN KEY (`updated_by`) REFERENCES admins(`id`) ON DELETE SET NULL,
+)
