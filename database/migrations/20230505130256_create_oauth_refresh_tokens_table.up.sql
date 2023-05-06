8f6f5471ca6b4ed4505d397899039e8e6d9a0950
Dzaky Mohammad <dzdzaakk@gmail.com>
2023-05-06 23:41:14 +0700

    add fields on table oauth refresh tokens

---
+++ database/migrations/20230505130256_create_oauth_refresh_tokens_table.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE oauth_access_tokens(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `oauth_access_token_id` INT NOT NULL,
+    `user_id` INT NOT NULL,
+    `token` VARCHAR(255) NOT NULL,
+    `expired_at` TIMESTAMP NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NULL,
+    `deleted_at` TIMESTAMP NULL,
+    PRIMARY KEY(`id`)
+    CONSTRAINT FK_oauth_access_token_id FOREIGN KEY (`oatuh_access_token_id`) REFERENCES oauth_access_tokens(`id`) ON DELETE SET NULL
+    
+)
