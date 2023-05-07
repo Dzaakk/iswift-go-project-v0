d4ae41b611d6a6e7946b0f0e2accc04177d1282d
Dzaky Mohammad <dzdzaakk@gmail.com>
2023-05-07 22:31:51 +0700

    fix some typo on table class rooms

--- database/migrations/20230505130426_create_class_rooms_table.up.sql
+++ database/migrations/20230505130426_create_class_rooms_table.up.sql
@@ -7,9 +7,9 @@ CREATE TABLE class_roooms(
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NULL,
     `deleted_at` TIMESTAMP NULL,
-    PRIMARY KEY(`id`)
+    PRIMARY KEY(`id`),
     CONSTRAINT FK_class_rooms_product_id FOREIGN KEY (`product_id`) REFERENCES products(`id`) ON DELETE SET NULL, 
-    CONSTRAINT FK_class_rooms_user_id FOREIGN KEY (`user_id`) REFERENCES users(`id`) ON DELETE SET NULL 
+    CONSTRAINT FK_class_rooms_user_id FOREIGN KEY (`user_id`) REFERENCES users(`id`) ON DELETE SET NULL, 
     CONSTRAINT FK_class_rooms_created_by FOREIGN KEY (`created_by`) REFERENCES admins(`id`) ON DELETE SET NULL, 
     CONSTRAINT FK_class_rooms_updated_by FOREIGN KEY (`updated_by`) REFERENCES admins(`id`) ON DELETE SET NULL,
 )
