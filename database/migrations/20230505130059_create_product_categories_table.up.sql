f908f84ad2e513f911e1319a4b888f6fff2c93a1
Dzaky Mohammad <dzdzaakk@gmail.com>
2023-05-10 18:04:00 +0700

    add coma and some index

--- database/migrations/20230505130059_create_product_categories_table.up.sql
+++ database/migrations/20230505130059_create_product_categories_table.up.sql
@@ -6,7 +6,9 @@ CREATE TABLE product_categories(
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NULL,
     `deleted_at` TIMESTAMP NULL,
-    PRIMARY KEY(`id`)
+    PRIMARY KEY(`id`),
+    INDEX idx_product_categories_created_by (`created_by`),
+    INDEX idx_product_categories_updated_by (`updated_by`),
     CONSTRAINT FK_product_categories_created_by FOREIGN KEY (`created_by`) REFERENCES admins(`id`) ON DELETE SET NULL,
     CONSTRAINT FK_product_categories_updated_by FOREIGN KEY (`updated_by`) REFERENCES admins(`id`) ON DELETE SET NULL
 )
