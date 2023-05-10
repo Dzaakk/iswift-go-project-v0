6cb25e78976583e21616aba948b68df86acbd34a
Dzaky Mohammad <dzdzaakk@gmail.com>
2023-05-10 18:05:09 +0700

    add some index

--- database/migrations/20230505130413_create_carts_table.up.sql
+++ database/migrations/20230505130413_create_carts_table.up.sql
@@ -1,10 +1,12 @@
 CREATE TABLE carts(
     `id` INT NOT NULL AUTO_INCREMENT,
-    `product_id` INT NOT NULL,
-    `user_id` INT NOT NULL,
+    `user_id` INT NULL,
+    `product_id` INT NULL,
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NULL,
     PRIMARY KEY(`id`),
+    INDEX idx_carts_user_id (`user_id`),
+    INDEX idx_carts_product_id (`product_id`),
     CONSTRAINT FK_class_rooms_product_id FOREIGN KEY (`product_id`) REFERENCES products(`id`) ON DELETE SET NULL, 
-    CONSTRAINT FK_class_rooms_user_id FOREIGN KEY (`user_id`) REFERENCES users(`id`) ON DELETE SET NULL, 
+    CONSTRAINT FK_class_rooms_user_id FOREIGN KEY (`user_id`) REFERENCES users(`id`) ON DELETE SET NULL
 )
--- database/migrations/20230505130508_create_forgot_passwords_table.up.sql
+++ database/migrations/20230505130508_create_forgot_passwords_table.up.sql
@@ -1,11 +1,12 @@
 CREATE TABLE forgot_passwords(
     `id` INT NOT NULL AUTO_INCREMENT,
-    `user_id` INT NOT NULL,
-    `valid` BOOLEAN NOT NULL,
-    `expired_at` TIMESTAMP,
+    `user_id` INT NULL,
+    `valid` BOOLEAN NOT NULL DEFAULT 1,
+    `expired_at` TIMESTAMP NULL,
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NULL,
     `deleted_at` TIMESTAMP NULL,
     PRIMARY KEY(`id`),
+    INDEX idx_forgot_passwords_user_id(`user_id`),
     CONSTRAINT FK_forgot_passwords_user_id FOREIGN KEY (`user_id`) REFERENCES users(`id`) ON DELETE SET NULL
 )
