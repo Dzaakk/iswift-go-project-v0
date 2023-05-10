735ceb512709b1fe39db5fcd32a65cbe31151d4c
Dzaky Mohammad <dzdzaakk@gmail.com>
2023-05-10 18:11:33 +0700

    register usecase fix day 2

--- internal/register/usecase/register_usecase.go
+++ internal/register/usecase/register_usecase.go
@@ -2,29 +2,43 @@ package register
 
 import (
 	dto "iswift-go-project/internal/register/dto"
-	userUseCase "iswift-go-project/internal/user/usecase"	
 	userDto "iswift-go-project/internal/user/dto"
+	userUseCase "iswift-go-project/internal/user/usecase"
 	mail "iswift-go-project/pkg/mail/sendgrid"
 )
 
 type RegisterUseCase interface {
-	Register(registerDto dto.CreateRegisterRequestBody) error
+	Register(userDto userDto.UserRequestBody) error
 }
 
 type RegisterUseCaseImpl struct {
-	//
-
+	userUseCase userUseCase.UserUseCase
+	mail        mail.Mail
 }
 
-func NewRegisterUseCase(userUseCase userUseCase.UserUseCase) RegisterUseCase {
-	userUseCase useuserUseCase.UserUseCase
-//
-	return &RegisterUseCaseImpl{userUseCase}
+func NewRegisterUseCase(
+	userUseCase userUseCase.UserUseCase,
+	mail mail.Mail,
+) RegisterUseCase {
+	return &RegisterUseCaseImpl{userUseCase, mail}
 }
 
 func (ru *RegisterUseCaseImpl) Register(userDto userDto.UserRequestBody) error {
 	// Check ke dalam module user
 	user, err := ru.userUseCase.Create(userDto)
 
-	
+	if err != nil {
+		return err
+	}
+
+	//kirim email melalui sendgrid
+	email := dto.CreateEmailVerification{
+		SUBJECT:           "Kode Verifikasi",
+		EMAIL:             user.Email,
+		VERIFICATION_CODE: user.CodeVerified,
+	}
+
+	go ru.mail.SendVerifivationEmail(user.Email, email)
+
+	return nil
 }
