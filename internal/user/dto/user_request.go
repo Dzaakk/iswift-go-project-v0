8717db128da640ce1a41f906d1f0d8e74fb4d600
Dzaky Mohammad <dzdzaakk@gmail.com>
2023-05-10 18:09:01 +0700

    validate and save

--- internal/user/dto/user_request.go
+++ internal/user/dto/user_request.go
@@ -1,8 +1,8 @@
 package user
 
 type UserRequestBody struct {
-	Name      *string `json:"name" binding`
-	Email     *string `json:"email"`
+	Name      *string `json:"name" binding:"required"`
+	Email     *string `json:"email" binding:"email"`
 	Password  *string `json:"password"`
 	CreatedBy *int64  `json:"created_by"`
 	UpdatedBy *int64  `json:"updated_by"`
