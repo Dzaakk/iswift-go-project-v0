d357b43b49740f19d3148c9ba5904104514a48cf
Dzaky Mohammad <dzdzaakk@gmail.com>
2023-05-17 22:11:07 +0700

    fix expired at and typo title

--- database/migrations/20230505130231_create_oauth_access_tokens_table.up.sql
+++ database/migrations/20230505130231_create_oauth_access_tokens_table.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE oauth_access_tokens(
     `user_id` INT NOT NULL,
     `token` VARCHAR(255) NULL,
     `scope` VARCHAR(255) NULL,
-    `expired_at` TIMESTAMP NOT NULL,
+    `expired_at` TIMESTAMP NULL,
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NULL,
     `deleted_at` TIMESTAMP NULL,
