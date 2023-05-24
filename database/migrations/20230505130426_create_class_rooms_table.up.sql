86c98ccf8e800cbe400eb3ea6d9926b6fd9f04a9
Dzkzzz <dzdzaakk@gmail.com>
2023-05-24 23:08:00 +0700

    fix typo

--- database/migrations/20230505130426_create_class_rooms_table.up.sql
+++ database/migrations/20230505130426_create_class_rooms_table.up.sql
@@ -8,12 +8,12 @@ CREATE TABLE class_rooms (
     `updated_at` TIMESTAMP NULL,
     `deleted_at` TIMESTAMP NULL,
     PRIMARY KEY (`id`),
-    INDEX idx_class_rooms_user_id (`user_id`) ,
-    INDEX idx_class_rooms_product_id (`product_id`) ,
-    INDEX idx_class_rooms_created_by (`created_by`) ,
-    INDEX idx_class_rooms_updated_by (`updated_by`) ,
+    INDEX idx_class_rooms_user_id (`user_id`),
+    INDEX idx_class_rooms_product_id (`product_id`),
+    INDEX idx_class_rooms_created_by (`created_by`),
+    INDEX idx_class_rooms_updated_by (`updated_by`),
     CONSTRAINT FK_class_rooms_user_id FOREIGN KEY (`user_id`) REFERENCES users(`id`)  ON DELETE SET NULL,
     CONSTRAINT FK_class_rooms_product_id FOREIGN KEY (`product_id`) REFERENCES products(`id`)  ON DELETE SET NULL,
-    CONSTRAINT FK_class_rooms_created_by FOREIGN KEY (`created_by`) REFERENCES admins(`id`)  ON DELETE SET NULL,
-    CONSTRAINT FK_class_rooms_updated_by FOREIGN KEY (`updated_by`) REFERENCES admins(`id`)  ON DELETE SET NULL
+    CONSTRAINT FK_class_rooms_created_by FOREIGN KEY (`created_by`) REFERENCES users(`id`)  ON DELETE SET NULL,
+    CONSTRAINT FK_class_rooms_updated_by FOREIGN KEY (`updated_by`) REFERENCES users(`id`)  ON DELETE SET NULL
 )
