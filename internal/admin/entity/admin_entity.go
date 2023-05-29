766d6baa4e624b42d95b13b4f7f5796c9c5e83bf
Dzkzzz <dzdzaakk@gmail.com>
2023-05-29 23:41:22 +0700

    change byids to inst64 and deletedat to gorm

--- internal/admin/entity/admin_entity.go
+++ internal/admin/entity/admin_entity.go
@@ -1,17 +1,21 @@
 package admin
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"gorm.io/gorm"
+)
 
 type Admin struct {
-	ID          int64        `json:"id"`
-	Email       string       `json:"email"`
-	Name        string       `json:"name"`
-	Password    string       `json:"password"`
-	CreatedBy   *Admin       `gorm:"foreignKey:CreatedByID;references:ID"`
-	CreatedByID string       `json:"created_by"`
-	UpdatedBy   *Admin       `gorm:"foreignKey:UpdatedByID;references:ID"`
-	UpdatedByID string       `json:"updated_by"`
-	CreatedAt   sql.NullTime `json:"created_at"`
-	UpdatedAt   sql.NullTime `json:"updated_at"`
-	DeletedAt   sql.NullTime `json:"deleted_at"`
+	ID          int64          `json:"id"`
+	Email       string         `json:"email"`
+	Name        string         `json:"name"`
+	Password    string         `json:"password"`
+	CreatedBy   *Admin         `gorm:"foreignKey:CreatedByID;references:ID"`
+	CreatedByID int64          `json:"created_by"`
+	UpdatedBy   *Admin         `gorm:"foreignKey:UpdatedByID;references:ID"`
+	UpdatedByID *int64         `json:"updated_by"`
+	CreatedAt   sql.NullTime   `json:"created_at"`
+	UpdatedAt   sql.NullTime   `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `json:"deleted_at"`
 }
