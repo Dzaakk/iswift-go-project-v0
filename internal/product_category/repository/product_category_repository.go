ac169c21132848cf8abd0f72972ddc7048ec32f2
Dzkzzz <dzdzaakk@gmail.com>
2023-05-31 21:58:23 +0700

    create product category repository

---
+++ internal/product_category/repository/product_category_repository.go
@@ -0,0 +1,70 @@
+package product_category
+
+import (
+	entity "iswift-go-project/internal/product_category/entity"
+	"iswift-go-project/pkg/utils"
+
+	"gorm.io/gorm"
+)
+
+type ProductcategoryRepository interface {
+	FindAll(offset int, limit int) []entity.ProductCategory
+	FindById(id int) (*entity.ProductCategory, error)
+	Create(entity entity.ProductCategory) (*entity.ProductCategory, error)
+	Update(entity entity.ProductCategory) (*entity.ProductCategory, error)
+	Delete(id int) error
+}
+
+type ProductcategoryRepositoryImpl struct {
+	db *gorm.DB
+}
+
+// Create implements ProductcategoryRepository.
+func (repository *ProductcategoryRepositoryImpl) Create(entity entity.ProductCategory) (*entity.ProductCategory, error) {
+	if err := repository.db.Create(&entity).Error; err != nil {
+		return nil, err
+	}
+	return &entity, nil
+}
+
+// Delete implements ProductcategoryRepository.
+func (repository *ProductcategoryRepositoryImpl) Delete(id int) error {
+	if err := repository.db.Delete(id).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// FindAll implements ProductcategoryRepository.
+func (repository *ProductcategoryRepositoryImpl) FindAll(offset int, limit int) []entity.ProductCategory {
+	var productCategories []entity.ProductCategory
+
+	repository.db.Scopes(utils.Paginate(offset, limit)).Find(&productCategories)
+
+	return productCategories
+}
+
+// FindById implements ProductcategoryRepository.
+func (repository *ProductcategoryRepositoryImpl) FindById(id int) (*entity.ProductCategory, error) {
+	var productCategory entity.ProductCategory
+
+	if err := repository.db.First(&productCategory, id).Error; err != nil {
+		return nil, err
+	}
+
+	return &productCategory, nil
+}
+
+// Update implements ProductcategoryRepository.
+func (repository *ProductcategoryRepositoryImpl) Update(entity entity.ProductCategory) (*entity.ProductCategory, error) {
+	if err := repository.db.Save(&entity).Error; err != nil {
+		return nil, err
+	}
+
+	return &entity, nil
+}
+
+func NewProductCategoryRepository(db *gorm.DB) ProductcategoryRepository {
+	return &ProductcategoryRepositoryImpl{db}
+}
