7f7d990bdf6aead19ba4e6dbb2afbdaabef261c2
Dzkzzz <dzdzaakk@gmail.com>
2023-06-02 18:39:56 +0700

    add product category services

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	admin "iswift-go-project/internal/admin/injector"
 	oauth "iswift-go-project/internal/oauth/injector"
+	productCategory "iswift-go-project/internal/product_category/injector"
 	profile "iswift-go-project/internal/profile/injector"
 	register "iswift-go-project/internal/register/injector"
 	mysql "iswift-go-project/pkg/db/mysql"
@@ -19,7 +20,8 @@ func main() {
 	oauth.InitializedService(db).Route(&r.RouterGroup)
 	profile.InitializedService(db).Route(&r.RouterGroup)
 	admin.InitializedService(db).Route(&r.RouterGroup)
-	
+	productCategory.InitializedService(db).Route(&r.RouterGroup)
+
 	r.Run()
 
 }
