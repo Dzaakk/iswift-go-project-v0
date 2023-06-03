acb384bcb0c4ffe0e2ca8abebfd2fee4dc2b01cb
Dzkzzz <dzdzaakk@gmail.com>
2023-06-03 21:38:49 +0700

    complete product repository

--- internal/product/repository/product_repository.go
+++ internal/product/repository/product_repository.go
@@ -16,17 +16,25 @@ type ProductRepository interface {
 }
 
 type ProductRepositoryImpl struct {
-	db gorm.DB
+	db *gorm.DB
 }
 
 // Create implements ProductRepository.
 func (repository *ProductRepositoryImpl) Create(entity entity.Product) (*entity.Product, error) {
+	if err := repository.db.Create(&entity).Error; err != nil {
+		return nil, err
+	}
 
+	return &entity, nil
 }
 
 // Delete implements ProductRepository.
 func (repository *ProductRepositoryImpl) Delete(entity entity.Product) error {
-	panic("unimplemented")
+	if err := repository.db.Delete(&entity).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // FindAll implements ProductRepository.
@@ -34,18 +42,30 @@ func (repository *ProductRepositoryImpl) FindAll(offset int, limit int) []entity
 	var products []entity.Product
 
 	repository.db.Scopes(utils.Paginate(offset, limit)).Find(&products)
+
+	return products
 }
 
 // FindById implements ProductRepository.
 func (repository *ProductRepositoryImpl) FindById(id int) (*entity.Product, error) {
-	panic("unimplemented")
+	var product entity.Product
+
+	if err := repository.db.First(&product, id).Error; err != nil {
+		return nil, err
+	}
+
+	return &product, nil
 }
 
 // Update implements ProductRepository.
 func (repository *ProductRepositoryImpl) Update(entity entity.Product) (*entity.Product, error) {
-	panic("unimplemented")
+	if err := repository.db.Save(&entity).Error; err != nil {
+		return nil, err
+	}
+
+	return &entity, nil
 }
 
-func NewProductRepository(db gorm.DB) ProductRepository {
+func NewProductRepository(db *gorm.DB) ProductRepository {
 	return &ProductRepositoryImpl{db}
 }
